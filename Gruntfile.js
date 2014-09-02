5c061941243acee59fbddff0b613892af086e197
Tom Johnson <seniorflexdeveloper@gmail.com>
2014-09-02 07:44:16 -0700

    fix grunt dist task to use src dir, was broken

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
         stripBanners: true
       },
       dist: {
-        src: 'lib/**/*.js',
+        src: 'src/*.js',
         dest: 'dist/<%= pkg.name %>.js'
       }
     },

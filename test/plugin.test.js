1783a346aed2124e93e537848eb288efb6aa074d
Greg Smith <gsmith@brightcove.com>
2016-01-05 16:40:16 -0500

    Add generators src and test files, side by side with existing

---
+++ test/plugin.test.js
@@ -0,0 +1,58 @@
+import document from 'global/document';
+
+import QUnit from 'qunit';
+import sinon from 'sinon';
+import videojs from 'video.js';
+
+import plugin from '../src/plugin';
+
+const Player = videojs.getComponent('Player');
+
+QUnit.test('the environment is sane', function(assert) {
+  assert.strictEqual(typeof Array.isArray, 'function', 'es5 exists');
+  assert.strictEqual(typeof sinon, 'object', 'sinon exists');
+  assert.strictEqual(typeof videojs, 'function', 'videojs exists');
+  assert.strictEqual(typeof plugin, 'function', 'plugin is a function');
+});
+
+QUnit.module('videojs-errors', {
+
+  beforeEach() {
+    this.fixture = document.getElementById('qunit-fixture');
+    this.video = document.createElement('video');
+    this.fixture.appendChild(this.video);
+    this.player = videojs(this.video);
+
+    // Mock the environment's timers because certain things - particularly
+    // player readiness - are asynchronous in video.js 5.
+    this.clock = sinon.useFakeTimers();
+  },
+
+  afterEach() {
+
+    // The clock _must_ be restored before disposing the player; otherwise,
+    // certain timeout listeners that happen inside video.js may throw errors.
+    this.clock.restore();
+    this.player.dispose();
+  }
+});
+
+QUnit.test('registers itself with video.js', function(assert) {
+  assert.expect(2);
+
+  assert.strictEqual(
+    Player.prototype.errors,
+    plugin,
+    'videojs-errors plugin was registered'
+  );
+
+  this.player.errors();
+
+  // Tick the clock forward enough to trigger the player to be "ready".
+  this.clock.tick(1);
+
+  assert.ok(
+    this.player.hasClass('vjs-errors'),
+    'the plugin adds a class to the player'
+  );
+});

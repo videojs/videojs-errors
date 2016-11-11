eb88f1ef94948a3f163274f18ec545f4c825e295
jforbes <jforbes@brightcove.com>
2016-11-11 16:26:04 -0500

    1.1.1

---
+++ dist/lang/en.js
@@ -0,0 +1,12 @@
+videojs.addLanguage('en', {
+  "No video has been loaded": "No video has been loaded",
+  "Could not download the video": "Could not download the video",
+  "The video connection was lost, please confirm you are connected to the internet": "The video connection was lost, please confirm you are connected to the internet",
+  "The video is bad or in a format that cannot be played on your browser": "The video is bad or in a format that cannot be played on your browser",
+  "This video is either unavailable or not supported in this browser": "This video is either unavailable or not supported in this browser",
+  "Error Code": "Error Code",
+  "Technical details": "Technical details",
+  "The video download was cancelled": "The video download was cancelled",
+  "The video you are trying to watch is encrypted and we do not know how to decrypt it": "The video you are trying to watch is encrypted and we do not know how to decrypt it",
+  "An unanticipated problem was encountered, check back soon and try again": "An unanticipated problem was encountered, check back soon and try again"
+});
